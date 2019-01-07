b280178d3c19435d91c668bceda9e7335d4b93cb
wrath <mihai.ciocirdel@hotmail.com>
2019-01-07 23:13:21 +0000

    Add Postgres DB in django settings

--- temporary_docker_images/project_maze/project_maze/settings.py
+++ temporary_docker_images/project_maze/project_maze/settings.py
@@ -73,13 +73,28 @@ WSGI_APPLICATION = 'project_maze.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
+if os.getenv('DOCKER_CONTAINER'):
+    POSTGRES_HOST = 'db'
+else:
+    POSTGRES_HOST = '127.0.0.1'
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'serious_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': POSTGRES_HOST,  # <-- this is new
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
